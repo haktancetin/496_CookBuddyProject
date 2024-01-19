5027ae8740aaadcef3bd214b79a155aba53b6c65
STUzun <sabahattintimuruzun@etu.edu.tr>
2024-01-19 19:33:17 +0300

    Chatbot Test

--- LLMPractice/LLMTest/chatbot.py
+++ LLMPractice/LLMTest/chatbot.py
@@ -1,11 +1,31 @@
 import os
 import chainlit as cl
-from langchain import HuggingFaceHub, PromptTemplate,LLMChain
-from getpass import getpass
-HUGGINGFACEHUB_API_TOKEN = getpass()
-os.environ['HUGGINGFACEHUB_API_TOKEN']=HUGGINGFACEHUB_API_TOKEN
+from langchain.chains import LLMChain
+from langchain_community.llms import HuggingFaceHub
+from dotenv import load_dotenv
 
-model_id="gpt2-medium"
-conv_model=HuggingFaceHub(huggingface_hub_api_token=os.environ['HUGGINGFACEHUB_API_TOKEN'],
+from langchain.prompts import PromptTemplate
+import getpass
+
+key=load_dotenv()
+
+model_id="flax-community/t5-recipe-generation"
+conv_model=HuggingFaceHub(
                           repo_id=model_id,
-                          model_kwargs={"temperature":0.8,"max_new_tokens":300})
+                          model_kwargs={"temperature":0.8,"max_new_tokens":150})
+template='''How to cook a meal using:
+{query}
+'''
+
+@cl.on_chat_start
+def main():
+    prompt = PromptTemplate(template=template, input_variables=['query'])
+
+    conv_chain = LLMChain(llm=conv_model, prompt=prompt, verbose=True)
+    cl.user_session.set("llm_chain", conv_chain)
+
+@cl.on_message
+async def main(message:str):
+    llm_chain=cl.user_session.get("llm_chain")
+    res = await llm_chain.acall(message.content,callbacks=[cl.AsyncLangchainCallbackHandler()])
+    await cl.Message(content=res["text"]).send()
