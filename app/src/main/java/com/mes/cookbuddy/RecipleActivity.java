632cd5d83e245f8ebf5c1ea745a5eb21a3aaa323
MelinEss <laliamelines@gmail.com>
2024-02-12 00:14:30 +0300

    When the screen was turned, the layout proportions were not correct. Bug fixed. Recipe like and dislike layout added.

---
+++ app/src/main/java/com/mes/cookbuddy/RecipleActivity.java
@@ -0,0 +1,14 @@
+package com.mes.cookbuddy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RecipleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reciple);
+    }
+}
