6e712f0562d33e9fa22d55e75fb31189e51b53a3
STUzun <sabahattintimuruzun@etu.edu.tr>
2024-03-06 23:17:22 +0300

    text to image generation

--- recipe_generator_app.py
+++ recipe_generator_app.py
@@ -3,6 +3,34 @@ import uuid
 from flask import Flask, jsonify, request
 from transformers import FlaxAutoModelForSeq2SeqLM
 from transformers import AutoTokenizer
+from IPython.display import Image, display
+
+import torch
+import io
+from PIL import Image
+from base64 import encodebytes
+
+import clip
+import os.path as osp
+import os, sys
+import torchvision.utils as vutils
+sys.path.insert(0, '../')
+from GALIP_Sampling.code.lib.utils import load_model_weights,mkdir_p
+from GALIP_Sampling.code.models.GALIP import NetG, CLIP_TXT_ENCODER
+
+device = 'cpu' # 'cpu' # 'cuda:0'
+CLIP_text = "ViT-B/32"
+clip_model, preprocess = clip.load("ViT-B/32", device=device)
+clip_model = clip_model.eval()
+
+text_encoder = CLIP_TXT_ENCODER(clip_model).to(device)
+netG = NetG(64, 100, 512, 256, 3, False, clip_model).to(device)
+path = 'GALIP_Sampling/code/saved_models/pretrained/pre_cc12m.pth'
+checkpoint = torch.load(path, map_location=torch.device('cpu'))
+netG = load_model_weights(netG, checkpoint['model']['netG'], multi_gpus=False)
+
+batch_size = 8
+noise = torch.randn((batch_size, 100)).to(device)
 
 MODEL_NAME_OR_PATH = "flax-community/t5-recipe-generation"
 tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME_OR_PATH, use_fast=True)
@@ -118,6 +146,53 @@ def get_recipe():
 
     return jsonify(generated_recipe)
 
+
+@app.route('/generate_recipe_and_image', methods=['GET'])
+def get_recipe_and_image():
+    items = request.args.getlist('ingredients')
+    generated = generation_function(items)
+    generated_recipe = {}
+    for text in generated:
+        sections = text.split("\n")
+        for section in sections:
+            section = section.strip()
+            if section.startswith("title:"):
+                section = section.replace("title:", "")
+                headline = "TITLE"
+            elif section.startswith("ingredients:"):
+                section = section.replace("ingredients:", "")
+                headline = "INGREDIENTS"
+            elif section.startswith("directions:"):
+                section = section.replace("directions:", "")
+                headline = "DIRECTIONS"
+
+            if headline == "TITLE":
+                generated_recipe["title"] = section.strip().capitalize()
+                captions = [generated_recipe["title"]]
+            elif headline == "INGREDIENTS":
+                generated_recipe["ingredients"] = [f"  - {i + 1}: {info.strip().capitalize()}" for i, info in
+                                enumerate(section.split("--"))]
+            else:
+                generated_recipe["directions"] = [f"  - {i + 1}: {info.strip().capitalize()}" for i, info in
+                                enumerate(section.split("--"))]
+
+            generated_recipe["id"] = uuid.uuid4().hex
+    with torch.no_grad():
+        for i in range(len(captions)):
+            caption = captions[i]
+            tokenized_text = clip.tokenize([caption]).to(device)
+            sent_emb, word_emb = text_encoder(tokenized_text)
+            sent_emb = sent_emb.repeat(batch_size, 1)
+            fake_imgs = netG(noise, sent_emb, eval=True).float()
+            name = f'{captions[i].replace(" ", "-")}'
+            vutils.save_image(fake_imgs.data, 'GALIP_Sampling/code/src/samples/%s.png' % (name), nrow=8, value_range=(-1, 1), normalize=True)
+        pil_img = Image.open('GALIP_Sampling/code/src/samples/%s.png' % (name), mode='r')  # reads the PIL image
+        byte_arr = io.BytesIO()
+        pil_img.save(byte_arr, format='PNG')
+        encoded_img = encodebytes(byte_arr.getvalue()).decode('ascii')
+        generated_recipe['image']=encoded_img
+    return jsonify(generated_recipe)
+    #return pil_img
 
 if __name__ == '__main__':
     app.run()
