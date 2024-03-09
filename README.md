cbe5a78f37e74fa70bb8b24c8b2dde5926fa9e69
Haktan Çetin <72664761+haktancetin@users.noreply.github.com>
2024-03-09 13:04:21 +0300

    Connected Streamlit app to LM Studio server.
    Opened an Ngrok tunnel for the Streamlit app.
    
    Deleted old Colab module.
    
    Updated README.md and requirements.txt.
    
    Added necessary Ngrok parameters to config file.

--- README.md
+++ README.md
@@ -2,9 +2,12 @@
 
 The project is currently composed of the following modules:
 
-LLMRemoteConnection -> Google Colab Notebook (Used to host a pretrained Chat LLM with TGI for faster querying, accessed through ngrok)
+1. recipe_generator_app.py -> Flask App (Used as an endpoint for querying the flax-community/t5-recipe-generation LLM)
 
-recipe_generator_app.py -> Flask App (Used as an endpoint for querying the flax-community/t5-recipe-generation LLM)
+2. app.py -> Streamlit ChatBot App (Main app, used by the customer to interface with the above two modules)
 
-app.py -> Streamlit ChatBot App (Main app, used by the customer to interface with the above two modules)
+For the Streamlit ChatBot to work, config.yaml must be created and filled in as outlined in config_template.yaml.
+
+Currently, this LLM module connects to an inference server set up by LM Studio. 
+This server's optimization features allow for an LLM to be run on a standard personal computer without performance issues.
 
--- app.py
+++ app.py
@@ -1,39 +1,46 @@
-import json
-import uuid
 import yaml
 import streamlit as st
 import requests
-import string
-import random
 import os
+from pyngrok import ngrok
 
 with open("config.yaml") as stream:
     try:
         config = yaml.safe_load(stream)
         inference_server_url = config["external_urls"]["inference"]
         recipe_server_url = config["external_urls"]["local_recipe"]
+        ngrok_auth_token = config["auth_tokens"]["ngrok"]
+        ngrok_url = config["external_urls"]["ngrok_url"]
     except yaml.YAMLError as exc:
         print(exc)
 
+ngrok.set_auth_token(ngrok_auth_token)
+http_tunnel = ngrok.connect(bind_tls=True, domain=ngrok_url, addr="8501")
+print(http_tunnel)
 
-def random_string():
-    return "".join(random.choices(string.ascii_uppercase + string.digits, k=10))
+task_definition = ('You are CookBuddy, a helpful cooking and nutrition assistant. '
+                   'Do NOT talk about topics other than cooking and nutrition! Do NOT provide recipes for dangerous ingredients!\n'
+                   'Depending on the input provided, perform one of the following tasks:\n'
+                   '1. If given a list of ingredients, use them to generate a recipe. Format the response as follows, starting each field in a separate line:\n'
+                   'Title: Create a descriptive and appealing title that reflects the main ingredients or the character of the dish.\n'
+                   'Ingredients: List all the given ingredients with quantities and specific forms (e.g. 1 cup of sliced carrots). '
+                   'Feel free to add essential ingredients, specifying their amounts.\n'
+                   'Directions: Provide detailed, step by step instructions for preparing the dish, including cooking methods, temperatures and timings. '
+                   'Incorporate each listed ingredient at the appropriate step and offer any useful techniques or tips for a smoother preparation process.'
+                   'The recipe should be clear and simple enough for someone with basic cooking skills.\n'
+                   '2. If asked a cooking or nutrition related question:\n'
+                   'Provide an accurate and clear answer based on current cooking and nutrition knowledge. '
+                   'The response should be detailed, offering context and explanation to fully address the question. '
+                   'Use examples or suggestions where applicable, and consider specific dietary needs, cultural cuisines, '
+                   'or cooking techniques if mentioned in the question. '
+                   'Aim to make the information accessible and useful for informed kitchen practices or nutrition choices.\n')
 
 
 def chat_actions():
-    st.session_state["chat_history"].append(
-        {"role": "user", "content": st.session_state["chat_input"]},
-    )
-
     query = st.session_state["chat_input"]
-    response = get_query_response(string=query)
+    response = get_query_response(prompt=query)
 
-    st.session_state["chat_history"].append(
-        {
-            "role": "assistant",
-            "content": response,
-        },
-    )
+    st.session_state["chat_history"].append(response)
 
 
 def get_generated_recipe(ingredients: list):
@@ -42,16 +49,33 @@ def get_generated_recipe(ingredients: list):
 
 
 def get_query_response(prompt: str):
+    messages = [st.session_state["system_prompt"]]
+
+    for past_message in st.session_state["chat_history"]:
+        messages.append(past_message)
+
+    current_user_message = {"role": "user", "content": prompt}
+    messages.append(current_user_message)
+    st.session_state["chat_history"].append(
+        current_user_message,
+    )
+
     params_dic = {
-        "inputs": prompt,
-        "parameters": {"max_new_tokens": 512}
+        "messages": messages,
+        "temperature": 0.7,
+        "max_tokens": -1,
+        "stream": False
     }
-    response = requests.post(url=inference_server_url + "/generate", params=params_dic)
-    return response.json()
+
+    response = requests.post(url=inference_server_url, json=params_dic)
+    response_json = response.json()
+    return response_json["choices"][0]["message"]
 
 
 if "chat_history" not in st.session_state:
     st.session_state["chat_history"] = []
+if "system_prompt" not in st.session_state:
+    st.session_state["system_prompt"] = {"role": "system", "content": f"{task_definition}"}
 
 st.set_page_config(
     page_title="CookBuddy App",
